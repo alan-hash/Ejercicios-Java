833e1274ce0df209c730d5679d2b6a94fc19b075
alan-hash <62110525+alan-hash@users.noreply.github.com>
2022-09-12 01:26:26 -0500

    Ordenamiento_Burbuja
    algoritmo de ordenamiento burbuja

--- Ordenamiento_Burbuja/src/com/company/Main.java
+++ Ordenamiento_Burbuja/src/com/company/Main.java
@@ -1,8 +1,41 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner leer=new Scanner(System.in);
+        int mayor=0,temp;
+        System.out.println("Ingresa numeros divididos por coma");
+        String texto= leer.nextLine();
+
+        String numerosEntxt[]=texto.split(",");
+        int cantidad= numerosEntxt.length;
+        int numeros[]=new int[cantidad];
+
+        for (int i = 0; i <cantidad ; i++) {
+            numeros[i]=Integer.parseInt(numerosEntxt[i]) ;
+        }
+        for (int i = 0; i <(cantidad-1); i++) {
+            for (int j = 0; j <(cantidad-1) ; j++) {
+                if (numeros[j]>numeros[j+1]){
+                    temp=numeros[j];
+                    numeros[j]=numeros[j+1];
+                    numeros[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("Numeros ordenados de forma acendente");
+        for (int i = 0; i <cantidad ; i++) {
+            System.out.print(numeros[i]+",");
+        }
+        System.out.println("");
+
+        System.out.println("Numeros ordenados de forma decendente");
+        for (int i = cantidad-1; i>=0; i=i-1) {
+            System.out.print(numeros[i]+",");
+        }
+
     }
 }
