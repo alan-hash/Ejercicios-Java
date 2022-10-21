08bdf1832873d11be12612f1fbe2cf81c8254848
Alan Gonzalez <lixoye530@gmail.com>
2022-10-21 09:04:39 -0500

    ArrayList
    Arreglos dinámicos con ArrayList

---
+++ Lista_Array/src/List_Array/List_Array.java
@@ -0,0 +1,74 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package List_Array;
+
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+
+/**
+ *
+ * @author soporte
+ */
+public class List_Array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner leer=new Scanner(System.in);
+        String msj=leer.nextLine();
+       
+        listadinamica();
+    }
+
+    private static void Listanormal() {
+       int Nelementos=Integer.parseInt((JOptionPane.showInputDialog("Ingresa el numero de elementos")));
+       String lista[]=new String[Nelementos];
+       
+       
+        for (int i = 0; i <Nelementos; i++) {
+           lista[i]=new String();
+           lista[i]=JOptionPane.showInputDialog("Ingresa el elemento "+(i+1)+" de la lista");
+        }
+        
+        
+        System.out.println("Resultados de la lisya normal");
+        for (int i = 0; i < Nelementos; i++) {  
+            System.out.println(lista[i]);
+        }
+       
+    }
+
+    private static void listadinamica() {
+        Scanner leer=new Scanner(System.in);
+        
+        ArrayList <String> lista=new ArrayList<String>();
+        char respuesta;
+        int valor;
+        
+        do{
+            System.out.println("ingresa el valor");
+            valor=leer.nextInt();
+            String valor2=String.valueOf(valor); 
+            lista.add(valor2);
+            System.out.println("Deseas seguir metiendo elementos  S/N");
+            respuesta=leer.next().charAt(0);
+            
+        }while(respuesta=='s' ||respuesta=='S');
+        
+        System.out.println("Resultados de la lista normal");
+        for (int i = 0; i < lista.size(); i++) {  
+            System.out.println(lista.get(i));
+        }
+      
+        
+    }
+    
+}
--- Polimorfismo/src/Clases/Vehiculo.java
+++ Polimorfismo/src/Clases/Vehiculo.java
@@ -1,32 +1,67 @@
 package Clases;
 
+/**
+ *
+ * @author Soporte
+ */
 public class Vehiculo {
 
+    /**
+     *
+     */
     protected String matricula;
 
+    /**
+     *
+     */
     protected String marca;
 
+    /**
+     *
+     */
     protected String modelo;
 
+    /**
+     *
+     * @param matricula
+     * @param marca
+     * @param modelo
+     */
     public Vehiculo(String matricula, String marca, String modelo) {
         this.matricula = matricula;
         this.marca = marca;
         this.modelo = modelo;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getMatricula() {
         return matricula;
     }
 
+    /**
+     *
+     * @param matricula
+     */
     public void setMatricula(String matricula) {
         this.matricula = matricula;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getMarca() {
         return marca;
     }
 
-       public String mostrarDatos() { 
+    /**
+     *
+     * @return
+     */
+    public String mostrarDatos() { 
         return "Matricula: "+ this.matricula +" Marca: "+ this.marca+" Modelo: "+ this.modelo ;
        
     }
